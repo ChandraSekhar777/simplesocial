6aba3671244be54179050df585a7d7a658961aca
Chandu <chandubobby592@gmail.com>
2020-08-28 13:55:55 +0530

    Post check

--- posts/forms.py
+++ posts/forms.py
@@ -12,4 +12,4 @@ class PostForm(forms.ModelForm):
         user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
         if user is not None:
-            self.fields["group"].queryset = models.Group.objects.filter(pk__in=user.groups.values_list("group__pk"))
+            self.fields["group"].queryset = models.Group.objects.filter(pk__in=user.group.values_list("group__pk"))
